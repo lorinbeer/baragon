da3e3ecd5ea49caf4d0619284038090be120a465
lorinbeer <lorin@adobe.com>
2013-04-30 22:26:22 -0700

    initial tracking of test suite

---
+++ test/create.tests.js
@@ -0,0 +1,24 @@
+
+
+describe("Baragon-Create Test Suite", function () {
+    it("Expects create with no arguments to yield stub issue", function () {
+
+    });
+    it("Expects json literal to be added to request data", function () {
+
+    });
+    it("Expects data file contents to be added to request data", function () {
+
+    });
+    it("Expects directly specified data to be added to request data", function () {
+
+    });
+
+    it("Expects json literal specified data to overwrite data file specified data", function () {
+
+    });
+
+    it("Expects directly specified data to overwrite data file specified data and json literal specified data", function () {
+
+    } 
+});
